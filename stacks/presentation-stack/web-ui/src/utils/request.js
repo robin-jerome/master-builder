001e50092c101dd0478803e5fc12d6050bfcb9b3
Robin Jerome <robinjer@amazon.com>
2020-04-09 16:58:12 -0400

    Dynamic reading of Api settings

--- stacks/presentation-stack/web-ui/src/utils/request.js
+++ stacks/presentation-stack/web-ui/src/utils/request.js
@@ -1,13 +1,8 @@
 import Amplify, { API } from "aws-amplify";
 import { retryWrapper } from "./index";
 
-const settings = window.apiGwSettings || {
-  cognitoIdentityPool: "us-east-1:cb4f7f5e-0725-41fb-bf13-410d8258774b",
-  cognitoUserPoolId: "us-east-1_8twvxp97N",
-  cognitoUserPoolClientId: "49cilmpftgeehm5r1gdrl8buqe",
-  region: "us-east-1",
-  apiGwEndpoint: "https://ln2dshjaoh.execute-api.us-east-1.amazonaws.com/Dev"
-};
+console.log(window);
+const settings = window.apiGwSettings || {};
 const region = settings.region || "eu-west-1";
 
 Amplify.configure({
